50ba1e0db1a5cb24a03295e92738d6534df0f410
tidiwids <JackEdmonds432@outlook.com>
2024-12-13 23:41:04 +0000

    D: Added player sprite + hitbox code

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill((0,0,0))
+        player = Player(x, y)
+        player.draw(screen)
         pygame.display.flip()
-
+        
+        dt = clock.tick(60) / 1000
 
 
 
